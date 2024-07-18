066dfdf12814c63e73728245cd1f42af910af511
David Brochart <david.brochart@gmail.com>
2024-07-18 18:18:38 +0200

    Fork declare

--- reacttrs/__init__.py
+++ reacttrs/__init__.py
@@ -1,3 +1,22 @@
-from .reactive import reactive  # noqa
+from __future__ import annotations
+
+from .reactive import Reactive as Reactive
+from .reactive import watch as watch
 
 __version__ = "0.1.4"
+
+__all__ = [
+    "Reactive",
+    "Int",
+    "Float",
+    "Bool",
+    "Str",
+    "Bytes",
+    "watch",
+]
+
+Int: type[Reactive[int]] = Reactive
+Float: type[Reactive[float]] = Reactive
+Bool: type[Reactive[bool]] = Reactive
+Str: type[Reactive[str]] = Reactive
+Bytes: type[Reactive[bytes]] = Reactive
--- tests/test_reactive.py
+++ tests/test_reactive.py
@@ -1,57 +1,81 @@
-from textwrap import dedent
+from typing import List, Tuple
 
-from reacttrs import reactive
-
-
-class Foo:
-
-    name = reactive("Paul")
-    age = reactive(33)
-    birth = reactive(1990)
-
-    def watch_name(self, old, new):
-        print(f"{old=}, {new=}")
-
-    def validate_name(self, name):
-        if name == "John":
-            print("Hey John!")
-        return name
-
-    def compute_age(self) -> int:
-        age = 2023 - self.birth
-        print(f"{age=}")
-        return age
+import reacttrs
+from reacttrs import Reactive, watch
 
 
-def test(capfd):
+def test_predefined():
+    class Foo:
+        my_int = reacttrs.Int(1)
+        my_float = reacttrs.Float(3.14)
+        my_bool = reacttrs.Bool(True)
+        my_str = reacttrs.Str("Foo")
+        my_bytes = reacttrs.Bytes(b"bar")
+
     foo = Foo()
+    assert foo.my_int == 1
+    assert foo.my_float == 3.14
+    assert foo.my_bool == True
+    assert foo.my_str == "Foo"
+    assert foo.my_bytes == b"bar"
 
-    foo.name = "John"
-    out, err = capfd.readouterr()
-    # watch_name is called first time unless `init=False`
-    assert out == dedent("""\
-        old='Paul', new='Paul'
-        Hey John!
-        old='Paul', new='John'
-    """)
 
-    foo.name = "Steve"
-    out, err = capfd.readouterr()
-    assert out == dedent("""\
-        old='John', new='Steve'
-    """)
+def test_validate():
+    class Foo:
+        positive = reacttrs.Int(0)
 
-    foo.age
-    out, err = capfd.readouterr()
-    # compute_age is called first time unless `init=False`
-    assert out == dedent("""\
-        age=33
-        age=33
-    """)
+        @positive.validate
+        def _validate_positive(self, value: int) -> int:
+            return max(0, value)
 
-    foo.birth = 1991
-    foo.age
-    out, err = capfd.readouterr()
-    assert out == dedent("""\
-        age=32
-    """)
+    foo = Foo()
+    foo.positive = -1
+    assert foo.positive == 0
+    foo.positive = 1
+    assert foo.positive == 1
+
+
+def test_watch() -> None:
+    changes0: List[Tuple[int, int]] = []
+    changes1: List[Tuple[int, int]] = []
+    changes2: List[Tuple[int, int]] = []
+    changes3: List[Tuple[int, int]] = []
+
+    class Foo:
+        value0 = reacttrs.Int(0)
+        value1 = reacttrs.Int(0)
+
+        @value0.watch
+        def _watch_value0(self, old: int, new: int) -> None:
+            changes0.append((old, new))
+
+        @value0.watch
+        def _watch_value1(self, old: int, new: int) -> None:
+            changes1.append((old, new))
+
+    foo0 = Foo()
+    foo0.value0 = 1
+    assert changes0 == [(0, 1)]
+    assert changes1 == [(0, 1)]
+    foo0.value0 = 2
+    assert changes0 == [(0, 1), (1, 2)]
+    assert changes1 == [(0, 1), (1, 2)]
+
+    def callback(obj: Foo, old: int, new: int) -> None:
+        changes3.append((old, new))
+
+    foo1 = Foo()
+    watch(foo0, "value1", callback)  # type: ignore[arg-type]
+    foo0.value1 = 3
+    foo1.value1 = 4
+    assert changes0 == [(0, 1), (1, 2)]
+    assert changes1 == [(0, 1), (1, 2)]
+    assert changes3 == [(0, 3)]
+
+
+def test_custom():
+    class Foo:
+        things = Reactive[List[str]](["foo", "bar"])
+
+    foo = Foo()
+    assert foo.things == ["foo", "bar"]
